5190c69fd2e6c9709253067b22a7ce4e61fb2003
tanim-k <tanimkhan877@gmail.com>
2022-02-25 22:08:05 +0600

    dom manipulation

---
+++ basic.js
@@ -0,0 +1,37 @@
+var deposit = 400;
+// condition 
+if(deposit > 500){
+
+}
+if(deposit< 200){
+    
+}
+if(deposit == 500){
+
+}
+if(deposit != 500){
+    
+}
+if(deposit >= 500){
+
+}
+if(deposit <= 500){
+
+}
+// array 
+const nums = [34,34,45,65,32,65,6533,3];
+const numberCount = nums.length;
+console.log(numberCount);
+nums.pop();
+nums.push(111);
+// check whethers 222 included in the array
+if (nums.indexOf(222) != -1){
+
+}
+if(nums.includes(222)){
+
+}
+function fullName(first, second) {
+    const name = first + ' ' + second;
+}
+const person = fullName('rahim', 'ali');
