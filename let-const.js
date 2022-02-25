6d70c2627448a29cda260aa59aca806066130eaf
tanim-k <tanimkhan877@gmail.com>
2022-02-25 22:38:09 +0600

    dom manipulation

---
+++ let-const.js
@@ -0,0 +1,20 @@
+let balance = 1230;
+balance = 2222;
+const userName = 'janpakhi potas potas';
+const weTogether = 'ami '+ userName;
+console.log(weTogether);
+const numbers = [23,34,555,66,66,3];
+let sum = 0;
+for (let i=0; i< numbers.length; i++){
+    const number = numbers[i];
+    // console.log(number);
+    sum = sum + number;
+    console.log(sum);
+}
+// numbers.push(6666666);
+console.log(numbers[3])
+// numbers.pop(6666666)
+numbers[3]= 5434;
+const student = {roll:101, name: 'miofiz', job:'intern'};
+student.name = 'moggfalhe'
+console.log(student.name)
