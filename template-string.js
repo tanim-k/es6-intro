2e4a0b2c7a0064e37f6f7858931476663b4ad809
tanim-k <tanimkhan877@gmail.com>
2022-02-26 07:59:42 +0600

    using back tip and \n

---
+++ template-string.js
@@ -0,0 +1,9 @@
+const priya = 'asif kabir'
+const meye = `meye tumi ki dukkho chino`;
+const kobita = "kobita tumi sopnocarini hoye khobor niyo na";
+const multiline = 'this is my first line. \n' +
+'this is my second line \n' +
+'this is my first line \n' +
+'this is my fourth line';
+console.log(multiline)
+
