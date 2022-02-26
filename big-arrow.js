3b80db645bb9341f88e0253bd571c99215d97224
tanim-k <tanimkhan877@gmail.com>
2022-02-26 09:48:00 +0600

    arrow fn more

---
+++ big-arrow.js
@@ -0,0 +1,17 @@
+// arrow function 
+// 1
+const add = (num1, num2) => num1+num2;
+const sum = add(22, 44)
+console.log('add:', sum)
+// 2
+const multiply = (num1, num2, num3) => num1+num2+num3;
+const result = multiply(22, 4, 4)
+console.log('multiply:', result);
+// 3
+const fiveTimes = num => num*5;
+const output = fiveTimes(4);
+console.log('5times:', output);
+// 4
+const tenTimes = (num) => num*10;
+const value10 = tenTimes(22);
+console.log('tentimes:', value10);
