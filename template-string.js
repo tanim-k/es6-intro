967a33f5b1488ae1b50642e03215af78c97afb4d
tanim-k <tanimkhan877@gmail.com>
2022-02-26 08:28:33 +0600

    using back tic amd template literals

--- template-string.js
+++ template-string.js
@@ -7,3 +7,24 @@ const multiline = 'this is my first line. \n' +
 'this is my fourth line';
 console.log(multiline)
 
+const multilineNew = `
+    this is my first line
+    this is my second line
+    this is my fourth line
+    this is my fifth line
+`;
+console.log(multilineNew);
+const count = 5;
+const old = '<h3 class="friend-name">friend-5</h3>'
+const old2 = '<h3 class="friend-name">friend-' + count + '</h3>'
+const new1 = `<h3 class="friend-name">friend-${count}</h3>`
+console.log(new1);
+friends = ['abul', 'kabul','jabul',' fabul'];
+const first = 'rahim';
+const second = 'khan'
+const oldFull = first + ' ' + second;
+const newFull = `this is ${first} ${second}. he has money ${friends.length*500}`;
+console.log(newFull);
+console.log(friends.length);
+
+
