43c19e24ebef5598621f2ede5aa651f82b5b0ed5
tanim-k <tanimkhan877@gmail.com>
2022-02-28 09:50:11 +0600

    let const default parameters

---
+++ summary-es6.js
@@ -0,0 +1,10 @@
+// let const 1:
+let person ='kulsum begun'
+person = 'madari lord';
+const furfura = 'furfura pir'
+// default parameters 2:
+const getName = (first, last='bokachu') => {
+    return first + ' ' + last;
+}
+getName('fardin');
+console.log(getName('fardin'));
